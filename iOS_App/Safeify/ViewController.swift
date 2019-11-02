0450e1ef28a824f3ecb3c3308f8b11e9f5d8a193
Jake Chasan <jakechasan@me.com>
2019-11-02 14:53:24 -0400

    Initial Xcode iOS File Commit

---
+++ iOS_App/Safeify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Safeify
+//
+//  Created by Jake Chasan on 11/2/19.
+//  Copyright © 2019 Jake Chasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
