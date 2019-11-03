8ef55a933f94e0a42902fddd0d2780791c9919cf
Aparimeya Taneja <at307@duke.edu>
2019-11-02 21:43:44 -0400

    juul cocoa pods

---
+++ iOS_App/Pods/Target Support Files/Pods-Safeify/Pods-Safeify-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Safeify : NSObject
+@end
+@implementation PodsDummy_Pods_Safeify
+@end
--- iOS_App/Safeify/AppDelegate.swift
+++ iOS_App/Safeify/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
+        FirebaseApp.configure()
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
